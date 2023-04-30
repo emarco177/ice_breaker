eae0ac48af1185270c6fc5cecc5265218f6a0774
Eden Marco <emarco177@gmail.com>
2023-04-30 17:28:34 +0300

    removed tool decorator

--- tools/tools.py
+++ tools/tools.py
@@ -1,9 +1,7 @@
-from langchain.agents import tool
 
 from langchain.utilities import SerpAPIWrapper
 
 
-@tool
 def get_profile_url(name: str):
     """Searches for Linkedin or twitter Profile Page."""
     search = SerpAPIWrapper()
