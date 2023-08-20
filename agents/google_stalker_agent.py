89ad27168804e87c9f88330b517c19587dd49a7d
AJ <talkingtoaj@gmail.com>
2023-08-20 22:35:12 +0300

    improve web text scraping

--- agents/google_stalker_agent.py
+++ agents/google_stalker_agent.py
@@ -15,7 +15,9 @@ from langchain.schema import (
     messages_from_dict,
     messages_to_dict,
 )
-
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import tiktoken
+from langchain.text_splitter import TokenTextSplitter
 
 def lookup(name: str) -> str:
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
@@ -40,59 +42,66 @@ def lookup(name: str) -> str:
 
     return agent.run(prompt_template.format_prompt(name_of_person=name))
 
-def google_scraper(query):
+def text_only(html_content:str, remove_whitespace:bool=False)->str:
+    soup = BeautifulSoup(html_content, 'html.parser')
+    body_text_only = soup.find_all(text=True) # returns a list of strings
+    # strip out all the html tags and javascript
+    body_text_only = [text for text in body_text_only if text.parent.name not in ['style', 'script', 'head', 'title', 'meta', '[document]']]
+    if remove_whitespace:
+        # remove all the newlines and extra spaces
+        body_text_only = [text.strip() for text in body_text_only]
+        # remove all the empty strings
+        body_text_only = [text for text in body_text_only if text]        
+    # convert the list of strings into a single string
+    body_text_only = " ".join(body_text_only)
+    return body_text_only
+
+def google_scraper(query, pages=2):
     search = SerpAPIWrapper()
     results_page = search.results(query)
     text_results = ""
     for result in results_page["organic_results"]:
         response = requests.get(result["link"])
         html_content = response.text
-        soup = BeautifulSoup(html_content, 'html.parser')
-        body_text_only = soup.find_all(text=True) # returns a list of strings
-        # strip out all the html tags and javascript
-        body_text_only = [text for text in body_text_only if text.parent.name not in ['style', 'script', 'head', 'title', 'meta', '[document]']]
-        # remove all the newlines and extra spaces
-        body_text_only = [text.strip() for text in body_text_only]
-        # remove all the empty strings
-        body_text_only = [text for text in body_text_only if text]        
-        # convert the list of strings into a single string
-        body_text_only = " ".join(body_text_only)
-        condensed_body_text = condense_for_search(query, body_text_only)
-        text_results += condensed_body_text
-        print("=!!")
-        print(body_text_only)
-        print("=>>")
-        print(condensed_body_text)
-        print("=EE")
-    # condense all the results to be less than 3500 tokens
-    if count_tokens(text_results) > 3500:
-        text_results = condense_for_search(query, text_results)
-        print("=!!!!!")
-        print(condensed_body_text)
-        print("=EEEEE")
-    return text_results
+        text_results += text_only(html_content)
+    return condense_for_query(query, text_results)
 
-def count_tokens(text):
+def count_tokens(text, use_tiktoken=False):
+    def tiktoken_len(text):
+        tokenizer = tiktoken.get_encoding('cl100k_base')     # use cl100k_base tokenizer for gpt-3.5-turbo and gpt-4
+        tokens = tokenizer.encode(
+            text,
+            disallowed_special=()
+        )
+        return len(tokens)
+    if use_tiktoken:
+        return tiktoken_len(text)
     return len(text.split(" "))
 
-def condense_for_search(query, text_results):   
-    # chunk the text into 2000 token chunks
-    # we can count how many tokens are in the text by splitting on spaces
-    tokens = text_results.split(" ")
-    # we want to split the text into chunks of 2000 tokens, so we'll iterate over these chunks
-    # and add them to a list
+
+# create the length function used by the RecursiveCharacterTextSplitter
+
+def condense_for_query(query, text_results, summary_guide="Write a summary of information from the following text as far as it gives us insights to the person mentioned in this query:"):   
+    text_splitter = TokenTextSplitter(
+        chunk_size = 3500,
+        chunk_overlap  = 20,
+    )
+    documents = text_splitter.split_text(text_results)
     summary = ""
-    count = 0
-    template = """Write a summary of the following text as far as any information it reveals about this query: <<{query}>>, the rest can be discarded. If there is no relevant text, give no response. ==={text}==="""
-    while count < len(tokens):
-        chunk = " ".join(tokens[count:count+2000])
-        llm_summary = get_llm_response(template.format(query=query, text=chunk))
+    template = """{summary_guide} <<{query}>>. Text:\n{text}"""
+    sanitized_template = """Convert the following text into a bulletpoint list of information that can be determined about the person<<{query}>>. Some sentences in this text report that information could not be found, you should continue reading and summarizing until the end of the text. Text:{text}"""
+    for doc in documents:
+        llm_summary = get_llm_response(template.format(query=query, text=doc, summary_guide=summary_guide))
         print("==?")
         print(llm_summary)
-        print("==?")
+        print("==?")        
         summary += llm_summary
-        count += 2000
-    return summary
+    # claims by the LLM that the text does not provide information about the person mentioned in the query confuse the LLM, so we remove them first
+    llm_summary_sanitized = get_llm_response(sanitized_template.format(query=query, text=summary))
+    print("==!")
+    print(llm_summary_sanitized)
+    print("==!")
+    return llm_summary_sanitized
 
 def get_llm_response(user_message):
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")    
