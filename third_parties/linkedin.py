9d3374b9dd183c743c07979902fa204e849077ca
Eden Marco <emarco177@gmail.com>
2024-04-07 09:52:15 +0300

    added mocks

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -27,3 +27,34 @@ def scrape_linkedin_profile(linkedin_profile_url: str):
             group_dict.pop("profile_pic_url")
 
     return data
+
+
+def scrape_linkedin_profile_mock(
+    linkedin_profile_url: str,
+):
+    """scrape information from Eden's LinkedIn profiles,
+    Manually scrape the information from the LinkedIn profile
+    https://www.linkedin.com/in/eden-marco/ through a mock gist file
+    """
+    linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/78233eb934aa9850b689471a604465b188e761a0/eden-marco.json"
+    response = requests.get(
+        linkedin_profile_url,
+        timeout=10,
+    )
+
+    data = response.json()
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+
+    return data
+
+
+if __name__ == "__main__":
+    print(scrape_linkedin_profile_mock(linkedin_profile_url=""))
--- third_parties/twitter.py
+++ third_parties/twitter.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import tweepy
+import requests
 
 load_dotenv()
 
@@ -34,5 +35,27 @@ def scrape_user_tweets(username, num_tweets=5):
     return tweet_list
 
 
+def scrape_user_tweets_mock(username="EdenEmarco177", num_tweets=5):
+    """
+    Scrapes pre made Edens's Github Gist file of tweets and returns them as a list of dictionaries.
+    Each dictionary has three fields: "time_posted" (relative to now), "text", and "url".
+    https://twitter.com/EdenEmarco177
+    """
+    EDEN_TWITTER_GIST = "https://gist.githubusercontent.com/emarco177/827323bb599553d0f0e662da07b9ff68/raw/57bf38cf8acce0c87e060f9bb51f6ab72098fbd6/eden-marco-twitter.json"
+    tweets = requests.get(EDEN_TWITTER_GIST, timeout=5).json()
+
+    tweet_list = []
+    for tweet in tweets:
+
+        tweet_dict = {}
+        tweet_dict["text"] = tweet["text"]
+        tweet_dict["url"] = f"https://twitter.com/{username}/status/{tweet['id']}"
+        tweet_list.append(tweet_dict)
+
+    return tweet_list
+
+
 if __name__ == "__main__":
-    print(scrape_user_tweets(username="EdenEmarco177"))
+
+    tweets = scrape_user_tweets_mock(username="EdenEmarco177")
+    print(tweets)
