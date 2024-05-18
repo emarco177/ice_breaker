59fb9b5843f32953f1e7dde6afbab79aa609f5bc
Eden Marco <emarco177@gmail.com>
2024-05-18 20:53:23 +0300

    updated to langchain 0.2.0

--- chains/custom_chains.py
+++ chains/custom_chains.py
@@ -1,4 +1,4 @@
-from langchain.chains import LLMChain
+from langchain_core.runnables import RunnableSequence
 from langchain_openai import ChatOpenAI
 from langchain.prompts.prompt import PromptTemplate
 
@@ -9,7 +9,7 @@ llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 llm_creative = ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
 
 
-def get_summary_chain() -> LLMChain:
+def get_summary_chain() -> RunnableSequence:
     summary_template = """
          given the information about a person from linkedin {information}, and twitter posts {twitter_posts} I want you to create:
          1. a short summary
@@ -28,7 +28,7 @@ def get_summary_chain() -> LLMChain:
     return summary_prompt_template | llm | summary_parser
 
 
-def get_interests_chain() -> LLMChain:
+def get_interests_chain() -> RunnableSequence:
     interesting_facts_template = """
          given the information about a person from linkedin {information}, and twitter posts {twitter_posts} I want you to create:
          3 topics that might interest them
@@ -46,7 +46,7 @@ def get_interests_chain() -> LLMChain:
     return interesting_facts_prompt_template | llm | topics_of_interest_parser
 
 
-def get_ice_breaker_chain() -> LLMChain:
+def get_ice_breaker_chain() -> RunnableSequence:
     ice_breaker_template = """
          given the information about a person from linkedin {information}, and twitter posts {twitter_posts} I want you to create:
          2 creative Ice breakers with them that are derived from their activity on Linkedin and twitter, preferably on latest tweets
