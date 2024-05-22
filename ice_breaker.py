c77b9b8cc3df8df38802d26373f052b435e2051f
SmW2021 <92540951+SmW2021@users.noreply.github.com>
2024-05-21 20:34:22 -0400

    update code

--- ice_breaker.py
+++ ice_breaker.py
@@ -9,7 +9,7 @@ from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
 def ice_break_with(name: str) -> str:
     linkedin_username = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
 
     summary_template = """
     given the Linkedin information {information} about a person I want you to create:
@@ -33,4 +33,4 @@ if __name__ == "__main__":
     load_dotenv()
 
     print("Ice Breaker Enter")
-    ice_break_with(name="Eden Marco")
+    ice_break_with(name="Eden Marco Udemy")
