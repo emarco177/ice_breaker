c984e24052ffa2937338df3fa9dc1dd661ddf03c
yadaab <abhishek.vn303@gmail.com>
2024-08-17 21:54:23 -0400

    test commit

--- ice_breaker.py
+++ ice_breaker.py
@@ -7,7 +7,7 @@ from chains.custom_chains import (
     get_ice_breaker_chain,
 )
 from third_parties.linkedin import scrape_linkedin_profile
-from third_parties.twitter import scrape_user_tweets, scrape_user_tweets_mock
+#from third_parties.twitter import scrape_user_tweets, scrape_user_tweets_mock
 from output_parsers import (
     Summary,
     IceBreaker,
@@ -18,25 +18,26 @@ from output_parsers import (
 def ice_break_with(
     name: str,
 ) -> Tuple[Summary, TopicOfInterest, IceBreaker, str]:
+    print ("Hello, world!!")
     linkedin_username = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
 
-    twitter_username = twitter_lookup_agent(name=name)
-    tweets = scrape_user_tweets(username=twitter_username)
+    #twitter_username = twitter_lookup_agent(name=name)
+    #tweets = scrape_user_tweets(username=twitter_username)
 
     summary_chain = get_summary_chain()
     summary_and_facts: Summary = summary_chain.invoke(
-        input={"information": linkedin_data, "twitter_posts": tweets},
+        input={"information": linkedin_data} #, "twitter_posts": tweets},
     )
 
     interests_chain = get_interests_chain()
     interests: TopicOfInterest = interests_chain.invoke(
-        input={"information": linkedin_data, "twitter_posts": tweets}
+        input={"information": linkedin_data} #, "twitter_posts": tweets}
     )
 
     ice_breaker_chain = get_ice_breaker_chain()
     ice_breakers: IceBreaker = ice_breaker_chain.invoke(
-        input={"information": linkedin_data, "twitter_posts": tweets}
+        input={"information": linkedin_data} #, "twitter_posts": tweets}
     )
 
     return (
