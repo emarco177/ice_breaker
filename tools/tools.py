25bf170232029ddf7310e9119e0abc74109db1af
emarco177 <44670213+emarco177@users.noreply.github.com>
2024-10-04 19:42:28 +0300

    Update tools.py

--- tools/tools.py
+++ tools/tools.py
@@ -5,4 +5,4 @@ def get_profile_url_tavily(name: str):
     """Searches for Linkedin or Twitter Profile Page."""
     search = TavilySearchResults()
     res = search.run(f"{name}")
-    return res[0]["url"]
+    return res
