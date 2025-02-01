83904cb0ab64694a6f70da5ffecfc8fb31634338
Eden Marco <emarco177@gmail.com>
2025-02-01 12:24:57 +0200

    updated to use scrapin

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -10,12 +10,14 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
     Manually scrape the information from the LinkedIn profile"""
 
     if mock:
-        linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/78233eb934aa9850b689471a604465b188e761a0/eden-marco.json"
+        linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/859ec7d786b45d8e3e3f688c6c9139d8/raw/32f3c85b9513994c572613f2c8b376b633bfc43f/eden-marco-scrapin.json"
         response = requests.get(
             linkedin_profile_url,
             timeout=10,
         )
     else:
+        # ENROLL WITH COUPON CODE: EDENMARCO
+        # For 20% Discount on all pricing
         api_endpoint = "https://api.scrapin.io/enrichment/profile"
         params = {
             "apikey": os.environ["SCRAPIN_API_KEY"],
@@ -27,16 +29,12 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
             timeout=10,
         )
 
-    data = response.json().get("person", response.json())
+    data = response.json().get("person")
     data = {
         k: v
         for k, v in data.items()
-        if v not in ([], "", "", None)
-        and k not in ["people_also_viewed", "certifications"]
+        if v not in ([], "", "", None) and k not in ["certifications"]
     }
-    if data.get("groups"):
-        for group_dict in data.get("groups"):
-            group_dict.pop("profile_pic_url")
 
     return data
 
@@ -44,6 +42,6 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
 if __name__ == "__main__":
     print(
         scrape_linkedin_profile(
-            linkedin_profile_url="https://www.linkedin.com/in/eden-marco/",
-        )
+            linkedin_profile_url="https://www.linkedin.com/in/eden-marco/"
+        ),
     )
